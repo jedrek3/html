5feec95dcb9fd8d465ec15f901a03bc9c2a0bf6b
unknown <jedrekknapikyt@gmail.com>
2020-07-20 07:13:54 +0200

    first

---
+++ precache-manifest.6164b25a0d179fb61e3bef958ee75d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c242f435cece74c9469958aa385a93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a752eff46fbc4da58dd",
+    "url": "/static/css/main.b14fa343.chunk.css"
+  },
+  {
+    "revision": "0ff6f34bee73a303a10f",
+    "url": "/static/js/2.1eb36f56.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.1eb36f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a752eff46fbc4da58dd",
+    "url": "/static/js/main.738d1455.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0c22d2b02fa257bcfabcfefec65bdf69",
+    "url": "/static/media/engine.0c22d2b0.jpg"
+  }
+]);
